3614771e9cc1cac9f2c3d2feb753b2b1a83f2fca
Camille Bouquet <bouquetcamille@hotmail.fr>
2016-01-31 11:53:31 +0100

    Add useless new menu item

--- kamitokaze-editor/src/com/kamitokaze/editor/ui/Editor.java
+++ kamitokaze-editor/src/com/kamitokaze/editor/ui/Editor.java
@@ -4,14 +4,17 @@ import java.awt.Dimension;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 
-public class Editor {
+public class Editor implements UIStrings {
 	JFrame frame;
 	JPanel container;
 	JMenuBar menuBar;
@@ -46,20 +49,44 @@ public class Editor {
 		menuBar = new JMenuBar();
 		frame.setJMenuBar(menuBar);
 		
-		JMenu menuFile = new JMenu("File");
+		JMenu menuFile = new JMenu(MENU_FILE);
 		menuBar.add(menuFile);
 		menuFile.setMnemonic('F');
 		
-		JMenuItem menuItemQuit = new JMenuItem("Quit");
-		menuItemQuit.addActionListener(new ActionListener()
+		JMenuItem menuItemQuit = createMenuItem(MENU_QUIT, new ActionListener()
 		{
 			public void actionPerformed(ActionEvent arg0)
 			{
 				System.exit(0);
 			}
-		});
-		menuItemQuit.setMnemonic('Q');
-		menuFile.add(menuItemQuit);
+		}, Integer.valueOf('Q'), KeyEvent.VK_Q);
 		
+		JMenuItem menuItemNew = createMenuItem(MENU_NEW, new ActionListener()
+		{
+			public void actionPerformed(ActionEvent arg0)
+			{
+				JOptionPane.showMessageDialog(
+						null,CODGINPHASE_COMINGSOON,
+						DIALOG_NEWMAP_TITLE, JOptionPane.INFORMATION_MESSAGE);
+			}
+		}, Integer.valueOf('N'), KeyEvent.VK_N);
+		
+		menuFile.add(menuItemNew);
+		menuFile.add(menuItemQuit);
+	}
+	
+	private JMenuItem createMenuItem(String title, ActionListener action, Integer mnemonic, Integer accelerator) {
+		JMenuItem menuItem = new JMenuItem(title);
+		menuItem.addActionListener(action);
+		
+		if (mnemonic != null) {
+			menuItem.setMnemonic(mnemonic);
+		}
+		
+		if (accelerator != null) {
+			menuItem.setAccelerator(KeyStroke.getKeyStroke(accelerator, KeyEvent.CTRL_MASK));
+		}
+		
+		return menuItem;
 	}
 }
---
+++ kamitokaze-editor/src/com/kamitokaze/editor/ui/UIStrings.java
@@ -0,0 +1,10 @@
+package com.kamitokaze.editor.ui;
+
+public interface UIStrings {
+	public String CODGINPHASE_COMINGSOON = "Coming soon...";
+	public String MENU_FILE = "File";
+	public String MENU_NEW = "New";
+	public String MENU_QUIT = "Quit";
+	
+	public String DIALOG_NEWMAP_TITLE = "Create new map";
+}
